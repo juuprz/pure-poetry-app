1f0bb2492585c68438a210b556e53c97db2e8efe
Julian Perez <juuprz@gmail.com>
2019-09-12 09:40:24 -0700

    Create basic template styling for Navabar component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
+    <Navbar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    Navbar,
   },
-};
+};  
 </script>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Pure Poetry</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Create A Poem<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">View All Poems</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="scss">
+  
+
+</style>
