aede78737734ed673c21b48b4dbb5c5441e0f7cc
Julian Perez <juuprz@gmail.com>
2019-09-11 18:10:41 -0700

    create controller to post poem in db and verify in postman

--- database/controllers/poems.js
+++ database/controllers/poems.js
@@ -1,1 +1,31 @@
 const Poem = require('../models/poems');
+
+// post
+const postPoem = (req, res) => {
+  Poem.create({ poem: req.body.poem }, (err, poem) => {
+    if (err) {
+      console.error('issue posting to the db', err);
+      res.sendStatus(404);
+    } else {
+      console.log('poem posted to the db', poem);
+      const reqInfo = {
+        id: poem._id,
+        createdAt: poem.createdAt,
+        updatedAt: poem.updatedAt,
+      };
+      res.status(401).send(JSON.stringify(reqInfo));
+    }
+  });
+};
+
+// GET
+// DELETE
+// const deletePoem = (req, res) => {
+//   Poem.deleteOne({})
+
+// }
+// PUT
+
+module.exports = {
+  postPoem,
+};
--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const app = express();
 const db = require('../database/index');
 require('../server/config/seed');
 
+const { postPoem } = require('../database/controllers/poems');
+
 app.use(bodyParser.json());
 app.use(express.static(dist));
+
+
+app.post('/api/post', postPoem);
 
 app.listen(PORT, () => console.log('this ya server listenin on', PORT));
